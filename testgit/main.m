bf7ab3c2b73575c3544c3d1f2887cdd7b157d9e7
orchisan <orchisan@163.com>
2016-01-14 23:16:13 +0800

    Initial Commit

---
+++ testgit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testgit
+//
+//  Created by David on 16/1/14.
+//  Copyright © 2016年 david. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
