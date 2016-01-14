0ce8b498a435646ce2481fd8fc989c68d91dacd3
orchisan <orchisan@163.com>
2016-01-14 23:18:57 +0800

    second

--- testgit/main.m
+++ testgit/main.m
@@ -12,6 +12,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSLog(@"test 2");
     }
     return 0;
 }
